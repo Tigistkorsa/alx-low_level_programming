4ca0448a3a326d440674a56482546b3230ecc011
TigistKorsa <tg5668k@gmail.com>
2022-12-08 11:52:08 -0800

    c hello word

--- 5-printf.c
+++ 5-printf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 /**
  *
  * *main - Entry point
@@ -14,10 +16,9 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-					return (0);
-
+		return (0);
 }
 
 
