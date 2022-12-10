0cd0749634fc94d836a6597edb722da09c2e7475
TigistKorsa <tg5668k@gmail.com>
2022-12-10 15:41:20 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,14 @@
 /**
  * main -print out size of diffrent data type in c
  *
- * return: it retun 0(sucess)i
+ * Return: it retun (sucess)
  */
 int main(void)
 {
 	printf("size of a char: %lu byte(S)\n", sizeof(char));
 	printf("size of a int: %lu byte(S)\n",  sizeof(int));
 	printf("size of a long int: %lu byte(S)\n", sizeof(long int));
-	printf("size of a long long int: %lu byte(S)\n", sizeof(long long int));	printf("size of a float: %lu byte(S)\n", sizeof(float));
+	printf("size of a llong int: %lu byte(S)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(S)\n", sizeof(float));
 	return (0);
 }
