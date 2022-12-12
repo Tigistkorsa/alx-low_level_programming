aba1073ca3623973254cf08c91b03b7575816c50
TigistKorsa <tg5668k@gmail.com>
2022-12-12 12:08:41 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- *  main -entry point
- *  Return :always 1
+ * main -display error msg and return value 1
+ *
+ * Return:is return for failuer
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
