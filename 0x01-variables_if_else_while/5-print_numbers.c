3d08919c8ad023d3a1f33009ae985a6252929483
TigistKorsa <tg5668k@gmail.com>
2022-12-12 16:01:53 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -print all stringd from 0-9
+ * Return:always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
