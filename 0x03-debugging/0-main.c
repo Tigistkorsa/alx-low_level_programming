4d6602c510278aa0ecd35e2cf330b976e3d06e31
TigistKorsa <tg5668k@gmail.com>
2022-12-20 12:49:46 -0800

    largest

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
- *main - main function check that prints if integer is positive or negative
- *Return: 0
+ * main - main function check that prints if integer is positive or negative
+ * Return: 0
  *
  */
 int main(void)
 {
-
 	int i;
 
 	i = 0;
@@ -14,5 +14,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }
