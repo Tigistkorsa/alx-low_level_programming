21477d167b0551cb3dee09a9d1f61dceec2fc58c
TigistKorsa <tg5668k@gmail.com>
2022-12-20 14:46:19 -0800

    advnace

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_times_table - prints time table with provided number
- * @n: the provided value by the user
+ *  print_times_table - prints time table with provided number
+ *  @n: the provided value by the user
  * Return: Always 0 (Success)
  */
 void print_times_table(int n)
 {
+
 	int row;
+
 	int column;
 
 	int product;
