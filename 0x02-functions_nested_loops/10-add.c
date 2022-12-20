3fc92b6154f3c2ede1acd910a1993f29d7d43a8a
TigistKorsa <tg5668k@gmail.com>
2022-12-20 14:17:00 -0800

    fixing

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Entry point for the addition
+ * @a: first value
+ * @b: second value
+ * Return: Always 0 (Success)
+ *
+ */
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
