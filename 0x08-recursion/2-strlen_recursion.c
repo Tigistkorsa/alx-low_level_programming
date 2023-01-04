d939335b276561a1e25b60f9699a010129155c4a
tigist korsa <tg5668k@gmail.com>
2023-01-04 13:52:33 -0800

    t5

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,26 +1,23 @@
 #include "main.h"
-
 /**
- * _strlen_recursion -  length of a string
- *
- * @s: the string to 
- *
- * Return: displays the length of the string
- *
- * */
+ *_strlen_recursion -length of a string
+ *@s:string
+ *Return: displays the length of the string
+ */
 int _strlen_recursion(char *s)
 
-{int length=0;
+{
+	int length = 0;
 
-	if(*s!= 0)
+	if (*s != 0)
 
 	{
 
 		length++;
 
-		length += _strlen_recursion(s+1);
+		length += _strlen_recursion(s + 1);
 
-		return length;
+		return (length);
 
 	}
 
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * checker-to get the square root of two numbers
+ * @a:first number
+ * @b:number to test for the square root of @a
+ * Return: square root
+ */
+
+int checker(int a, int b)
+
+{
+
+	if (b * b > a)
+
+		return (-1);
+
+	else if (b * b == a)
+
+		return (b);
+
+	else
+
+		return (checker(a, b + 1));
+
+	return (1);
+
+}
+
+int _sqrt_recursion(int n)
+
+{
+
+	if (n == 0)
+
+		return (0);
+
+	return (checker(n, 1));
+
+}
+
+	}
