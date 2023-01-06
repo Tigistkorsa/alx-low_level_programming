40d14c244848c4f261e1045a496d88a130088a5b
tigistkorsa <tg5668k@yahoo.com>
2023-01-06 22:46:40 +0300

    t0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower -check if a chracter is uppercase or lower case
+ *@c:the charcter
+ * Return: 1 if letter is lowercase .o if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
