2127086202a23cb92fe1f3eaed9a452462e8b8b6
tigist korsa <tg5668k@gmail.com>
2023-11-15 11:40:44 -0800

    q1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _puts_recursion -FUNCTION RECURSIVE
+* @s: character string
+* Return: returns nothing
+* Author: mgemoraw
+*/
+void _puts_recursion(char *s)
+{
+int j = 0;
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*(s + j));
+j++;
+_puts_recursion((char *)(s + j));
+}
+
+
