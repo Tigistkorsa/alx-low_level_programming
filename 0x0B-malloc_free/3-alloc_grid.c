aac0f0e98909d1199d9f741fd0b1481965a82942
tigist korsa <tg5668k@gmail.com>
2023-11-21 10:56:08 -0800

    task5

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,23 +9,28 @@
 */
 int **alloc_grid(int width, int height)
 {
+/*Declaring variables*/
 int **grid;
 int i, j;
 
 if (width <= 0 || height <= 0)
+{
 return (NULL);
+}
 
-grid = malloc(sizeof(int *) * height);
+grid = malloc(sizeof(int *) * height); /*malloc*/
 
 if (grid == NULL)
+{
 return (NULL);
+}
 
 for (i = 0; i < height; i++)
 {
 grid[i] = malloc(sizeof(int) * width);
 if (grid[i] == NULL)
 {
-for (i = i - 1; i > 0; i--)
+for (i = i - 1; i >= 0; i--)
 {
 free(grid[i]);
 }
@@ -33,14 +38,7 @@ free(grid);
 return (NULL);
 }
 }
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-{
+for (i = 0; j < width; j++)
 grid[i][j] = 0;
-}
-}
 return (grid);
 }
-
-
